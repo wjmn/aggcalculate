3fb293680f9bdb83534e5ad3e321aeffaf5557d6
wjmn <jamin.wu07@gmail.com>
2025-07-10 22:35:48 +0930

    Updates

---
+++ precache-manifest.276c6371954309cbc2533a0bb2a27f5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f5922a510df9a0c10d1ed754d954c0",
+    "url": "https://wjmn.github.io/aggcalculate/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "https://wjmn.github.io/aggcalculate/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "https://wjmn.github.io/aggcalculate/manifest.json"
+  },
+  {
+    "revision": "babbc17c1faa7a7d32b4",
+    "url": "https://wjmn.github.io/aggcalculate/static/css/main.2be63878.chunk.css"
+  },
+  {
+    "revision": "babbc17c1faa7a7d32b4",
+    "url": "https://wjmn.github.io/aggcalculate/static/js/main.babbc17c.chunk.js"
+  },
+  {
+    "revision": "a63d4e9646f5bfb41f95",
+    "url": "https://wjmn.github.io/aggcalculate/static/js/runtime~main.a63d4e96.js"
+  },
+  {
+    "revision": "ded87515c392c3502f6c",
+    "url": "https://wjmn.github.io/aggcalculate/static/js/vendors~main.ded87515.chunk.js"
+  }
+]);
